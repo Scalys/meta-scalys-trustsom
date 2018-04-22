85df87a6218f91d36f0860524d45cd3619fdced9
Dmitry Lavnikevich <dmitry.lavnikevich@edality.by>
2018-04-22 16:03:10 +0300

    bootscripts: Fix mtdparts bootarg

--- recipes-bsp/u-boot-bootscripts/files/boot-mmc.cmd
+++ recipes-bsp/u-boot-bootscripts/files/boot-mmc.cmd
@@ -1,4 +1,5 @@
 ext4load mmc 0:1 $kernel_addr_r /boot/uImage
 ext4load mmc 0:1 $fdt_addr_r /boot/grapeboard.dtb
-setenv bootargs 'console=ttyS0,115200 earlycon=uart8250,mmio,0x21c0500 lpj=250000 noinitrd root=/dev/mmcblk0p1 rootfstype=ext4 rw rootwait $mtdparts'
+mtdparts
+setenv bootargs "console=ttyS0,115200 earlycon=uart8250,mmio,0x21c0500 lpj=250000 noinitrd root=/dev/mmcblk0p1 rootfstype=ext4 rw rootwait $mtdparts"
 bootm $kernel_addr_r - $fdt_addr_r
--- recipes-bsp/u-boot-bootscripts/files/boot-qspi.cmd
+++ recipes-bsp/u-boot-bootscripts/files/boot-qspi.cmd
@@ -2,5 +2,5 @@ ubi part UBI
 ubifsmount ubi0:boot
 ubifsload $kernel_addr_r /boot/uImage
 ubifsload $fdt_addr_r /boot/grapeboard.dtb
-setenv bootargs 'console=ttyS0,115200 earlycon=uart8250,mmio,0x21c0500 lpj=250000 noinitrd ubi.mtd=4 root=ubi0:boot rootfstype=ubifs rw rootwait $mtdparts'
+setenv bootargs "console=ttyS0,115200 earlycon=uart8250,mmio,0x21c0500 lpj=250000 noinitrd ubi.mtd=4 root=ubi0:boot rootfstype=ubifs rw rootwait $mtdparts"
 bootm $kernel_addr_r - $fdt_addr_r
